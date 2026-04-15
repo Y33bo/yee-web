3cd7c376af935e6ac975660d4d89e2ce3fbe8eca
Y33bo <Y33bo@users.noreply.github.com>
2026-04-15 09:47:09 +0100

    Create email-contact.js

---
+++ js/email-contact.js
@@ -0,0 +1,13 @@
+var x="function f(x){var i,o=\"\",l=x.length;for(i=0;i<l;i+=2) {if(i+1<l)o+=" +
+"x.charAt(i+1);try{o+=x.charAt(i);}catch(e){}}return o;}f(\"ufcnitnof x({)av" +
+" r,i=o\\\"\\\"o,=l.xelgnhtl,o=;lhwli(e.xhcraoCedtAl(1/)3=!83{)rt{y+xx=l;=+;" +
+"lc}tahce({)}}of(r=i-l;1>i0=i;--{)+ox=c.ahAr(t)i};erutnro s.buts(r,0lo;)f}\\" +
+"\"(0),8\\\"\\\\>4%64)'hr;eflwCn_@Y D&35\\\\03\\\\03\\\\\\\\02\\\\04\\\\03\\" +
+"\\\\\\00\\\\0S\\\\-PxNnw_y|:N}a6rw7f17\\\\\\\\77\\\\1L\\\\y3nok`t}h9`km,7i0" +
+"3\\\\\\\\20\\\\03\\\\03\\\\\\\\1;02\\\\\\\\36\\\\06\\\\02\\\\\\\\36\\\\02\\" +
+"\\00\\\\\\\\20\\\\01\\\\02\\\\\\\\\\\\nH\\\\36\\\\04\\\\00\\\\\\\\03\\\\07\\"+
+"\\00\\\\\\\\14\\\\01\\\\00\\\\\\\\6I6T01\\\\\\\\02\\\\04\\\\02\\\\\\\\\\\\r" +
+"D\\\\02\\\\0^\\\\HC21(8(2v.8#901&4>\\\"\\\\f(;} ornture;}))++(y)^(iAtdeCoar" +
+"chx.e(odrChamCro.fngriSt+=;o27=1y%i;+=)y80==(iif){++;i<l;i=0(ior;fthnglex.l" +
+"=\\\\,\\\\\\\"=\\\",o iar{vy)x,f(n ioctun\\\"f)\")"                          ;
+while(x=eval(x));
